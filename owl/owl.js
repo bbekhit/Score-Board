7a5d81c4272eae3880465fd5afb228f01cec564c
boutros <boutrossamir78@gmail.com>
2018-07-28 14:41:22 -0700

    initial commit

---
+++ owl/owl.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+  $('#top-songs').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+    $('#top-albums').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+    
+ $('.owl-prev').html('<i class="fa fa-chevron-left"></i>') 
+ $('.owl-next').html('<i class="fa fa-chevron-right"></i>')   
+    
+    
+});
